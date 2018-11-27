9ac9fbc8d1af40b261f04f3e15ef8d8c4a344cd0
Sebastian Schoebinger <>
2018-11-27 18:03:26 +0100

    feat: init

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/asdf.html");
+});
+
+app.post("/sendPush", (req, res) => {
+  io.emit("wdw", true);
+  setTimeout(() => io.emit("wdw", false), 5000);
+  res.send();
+});
+
+io.on("connection", socket => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT || 4000, () => {
+  console.log(
+    `The server is running: http://localhost:${process.env.PORT || 4000}`
+  );
+});
