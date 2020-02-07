2d20f9724620128258c9f2ed282dc3e913bd198d
Sebastian Schoebinger <sebastian@macbookpro.local>
2020-02-07 14:29:48 +0100

    feat: fix speach

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server, { serveClient: false });
-const findcity = require("find-nearest-cities");
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, { serveClient: false })
+const findcity = require('find-nearest-cities')
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
-app.post("/sendPush", (req, res) => {
-  let push = req.body;
+app.post('/sendPush', (req, res) => {
+  let push = req.body
+  console.log(push)
   const cities = findcity(
     req.body.location.gps.latitude,
     req.body.location.gps.longitude
-  );
-  push["cities"] = cities.reduce(
+  )
+  push['cities'] = cities.reduce(
     (sum, city) => {
-      sum.count++;
-      sum.text += city.name + " ";
-      return sum;
+      sum.count++
+      sum.text += city.name + ', '
+      return sum
     },
-    { count: 0, text: "" }
-  ).text;
-  io.emit("wdw", push);
-  setTimeout(() => io.emit("wdw", undefined), 5000);
-  res.send();
-});
+    { count: 0, text: '' }
+  ).text
+  console.log(push)
+  io.emit('wdw', push)
+  setTimeout(() => io.emit('wdw', undefined), 5000)
+  res.send()
+})
 
-server.listen(process.env.PORT || 4000);
+server.listen(process.env.PORT || 4000)
